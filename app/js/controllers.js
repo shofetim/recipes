2a5f2ba3563f83200b57c3830ab620383231d6d2
Jordan Schatz <jordan@noinlabs.com>
2013-12-24 10:25:14 -0800

    WIP, working towards functioning form

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,49 +1,61 @@
 'use strict';
 
 /* Controllers */
+var recipes = [
+    {
+        'id': 1,
+        'title': 'Misoyaki Salmon',
+        'ingredients': [
+            {'ingredient': '1/4 cup white miso paste'},
+            {'ingredient': '2 tbsps naturally fermented soy sauce'},
+            {'ingredient': '2 tbsps honey'},
+            {'ingredient': '4 4- to 6-oz salmon fillets'},
+            {'ingredient': '2 tbsps sesame seeds'}
+        ],
+        'method' : 'Preheat oven to 425º F and line a baking sheet with parchment paper. Whisk miso paste, soy sauce and honey together in a mixing bowl until thoroughly combined. Place salmon filets on a baking sheet lined with parchment paper. Spoon miso, soy and honey paste over the salmon filets so that they’re fully covered. Sprinkle salmon with sesame seeds and bake in an oven preheated to 425º Fahrenheit for 15 to 20 minutes or until the fish flakes easily when pierced by a fork.',
+        'created_at': '2013-12-25T18:11:41.553Z'
+    },
+    {
+        'id': 2,
+        'title': 'Mayonnaise',
+        'ingredients': [
+            {'ingredient': '2 egg yolks'},
+            {'ingredient': '1 tbsp apple cider vinegar'},
+            {'ingredient': '1 tbsp water'},
+            {'ingredient': '1/4 tsp sea salt'},
+            {'ingredient': '3/4 cup olive oil'},
+        ],
+        'method' : 'Toss egg yolks, cider vinegar, water and unrefined sea salt into the basin of a food processor. Pulse 2 or 3 times until well-combined. In a smooth a steady stream, pour in olive oil and continue pouring slowly until the mayonnaise is thoroughly emulsified.',
+        'created_at': '2013-12-24T18:11:41.553Z'
+    },
+];
 
 angular.module('recipe.controllers', []).
   controller('ListCtrl', ['$scope', function($scope) {
-      $scope.recipes = [
-          {
-              'id': 1,
-              'title': 'Mayonnaise',
-              'ingredients': [
-                  '2 egg yolks',
-                  '1 tbsp apple cider vinegar',
-                  '1 tbsp water',
-                  '1/4 tsp unrefined sea salt',
-                  '3/4 cup unrefined extra virgin olive oil'
-              ],
-              'method': 'Toss egg yolks, cider vinegar, water and unrefined sea salt into the basin of a food processor. Pulse 2 or 3 times until well-combined. In a smooth a steady stream, pour in olive oil and continue pouring slowly until the mayonnaise is thoroughly emulsified.'
-          },
-          {
-              'id': 2,
-              'title': 'Misoyaki Salmon',
-              'ingredients': [
-                  '1/4 cup white miso paste',
-                  '2 tbsps naturally fermented soy sauce',
-                  '2 tbsps honey',
-                  '4 4- to 6-oz salmon fillets',
-                  '2 tbsps sesame seeds'
-              ],
-              'method' : 'Preheat oven to 425º F and line a baking sheet with parchment paper. Whisk miso paste, soy sauce and honey together in a mixing bowl until thoroughly combined. Place salmon filets on a baking sheet lined with parchment paper. Spoon miso, soy and honey paste over the salmon filets so that they’re fully covered. Sprinkle salmon with sesame seeds and bake in an oven preheated to 425º Fahrenheit for 15 to 20 minutes or until the fish flakes easily when pierced by a fork.'
-          }
-      ];
-      $scope.orderProp = 'name';
+      $scope.recipes = recipes;
+      $scope.orderProp = 'title';
   }])
   .controller('DetailCtrl', ['$scope', '$routeParams',
      function($scope, $routeParams) {
-         $scope.recipe = {
-             'id': 1,
-             'title': 'Mayonnaise',
+         $scope.new = {
+             'title': '',
              'ingredients': [
-                 '2 egg yolks',
-                 '1 tbsp apple cider vinegar',
-                 '1 tbsp water',
-                 '1/4 tsp unrefined sea salt',
-                 '3/4 cup unrefined extra virgin olive oil'
+                 {'ingredient': ''},
+                 {'ingredient': ''},
+                 {'ingredient': ''},
+                 {'ingredient': ''},
+                 {'ingredient': ''},
              ],
-             'method': 'Toss egg yolks, cider vinegar, water and unrefined sea salt into the basin of a food processor. Pulse 2 or 3 times until well-combined. In a smooth a steady stream, pour in olive oil and continue pouring slowly until the mayonnaise is thoroughly emulsified.'
+             'method': ''
          };
+         $scope.addIngredient = function() {
+             $scope.recipe.ingredients.push({'ingredient':''});
+         };
+         $scope.removeContact = function(index) {
+             $scope.recipe.ingredients.splice(index, 1);
+         };
+         $scope.save = function() {
+             console.log('implement me');
+         };
+         $scope.recipe = recipes[parseInt($routeParams['recipeId']) - 1];
   }]);
